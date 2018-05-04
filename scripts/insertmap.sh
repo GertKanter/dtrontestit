5c30dd69b6cb499b6b67372e9d129d36a1f63469
arturgummel <arturgummel@gmail.com>
2018-05-04 17:14:02 +0300

    path fix in files

--- scripts/insertmap.sh
+++ scripts/insertmap.sh
@@ -6,4 +6,4 @@ roscore & \
 sleep 5
 roslaunch mongodb_store mongodb_store.launch db_path:=/opt/ros/mongodb_store & \
 sleep 10
-rosrun topological_utils insert_map.py /catkin_ws/map.tmap map map
+rosrun topological_utils insert_map.py /catkin_ws/dtronpack/generateModelFromTmap/examples/map.tmap map map
--- turtlebot/scripts/run_top_nav.sh
+++ turtlebot/scripts/run_top_nav.sh
@@ -10,5 +10,5 @@ roslaunch dtron topological_navigation.launch map:=map &
 
 sleep 30
 echo "Start adapter"
-cd /catkin_ws/src/testit/dtron/turtlebot
+cd /catkin_ws/src/testit/dtron/turtlebot/scripts
 source run_adapter.sh config_top_nav.yaml
