562b9199442a88a852e15bdfb0859cc4d3a086fb
arturgummel <arturgummel@gmail.com>
2018-06-07 00:04:35 +0300

    mongodb fix

--- scripts/insertmap.sh
+++ scripts/insertmap.sh
@@ -2,8 +2,6 @@
 
 source /catkin_ws/devel/setup.bash
 
-roscore & \
-sleep 5
 roslaunch mongodb_store mongodb_store.launch db_path:=/opt/ros/mongodb_store & \
 sleep 10
 rosrun topological_utils insert_map.py /catkin_ws/dtronpack/generateModelFromTmap/examples/map.tmap map map
